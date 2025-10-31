2bcb5b8a309d40d9a673e8d48f4fd474056ce678
GJ Tiquia <gershom.tiquia@gmail.com>
2025-10-31 10:58:45 -0400

    change to checking out from branch instead of downloading source.tar.gz

--- bin/build-ghostty.sh
+++ bin/build-ghostty.sh
@@ -3,10 +3,11 @@
 set -eux
 
 ARCH="$(uname -m)"
-GHOSTTY_VERSION="$(cat VERSION)"
-PUB_KEY="RWQlAjJC23149WL2sEpT/l0QKy7hMIFhYdQOFy0Z7z7PbneUgvlsnYcV"
+GHOSTTY_REPO="https://github.com/pluiedev/ghostty"
+GHOSTTY_BRANCH="pluie/jj-uuurvsomytmv"
+GHOSTTY_DIR="ghostty"
 
-rm -rf AppDir dist ghostty-*
+rm -rf AppDir dist "${GHOSTTY_DIR}"
 
 BUILD_ARGS="
 	-Dcpu=baseline \
@@ -18,30 +19,15 @@ BUILD_ARGS="
     -Demit-docs=false \
     -Dstrip=true"
 
-if [ "${GHOSTTY_VERSION}" = "tip" ]; then
-	export UPINFO="gh-releases-zsync|$(echo "${GITHUB_REPOSITORY}" | tr '/' '|')|tip|Ghostty-*$ARCH.AppImage.zsync"
-	wget "https://github.com/ghostty-org/ghostty/releases/download/tip/ghostty-source.tar.gz" -O "ghostty-${GHOSTTY_VERSION}.tar.gz"
-	wget "https://github.com/ghostty-org/ghostty/releases/download/tip/ghostty-source.tar.gz.minisig" -O "ghostty-${GHOSTTY_VERSION}.tar.gz.minisig"
-	GHOSTTY_VERSION="$(tar -tf "ghostty-${GHOSTTY_VERSION}.tar.gz" --wildcards "*zig.zon.txt" | awk -F'[-/]' '{print $2"-"$3}')"
-	echo "${GHOSTTY_VERSION}" >VERSION
-	mv ghostty-tip.tar.gz "ghostty-${GHOSTTY_VERSION}.tar.gz"
-	mv ghostty-tip.tar.gz.minisig "ghostty-${GHOSTTY_VERSION}.tar.gz.minisig"
-else
-	wget "https://release.files.ghostty.org/${GHOSTTY_VERSION}/ghostty-${GHOSTTY_VERSION}.tar.gz"
-	wget "https://release.files.ghostty.org/${GHOSTTY_VERSION}/ghostty-${GHOSTTY_VERSION}.tar.gz.minisig"
-fi
+# Clone the repository and checkout the specific branch
+git clone --depth 1 --branch "${GHOSTTY_BRANCH}" "${GHOSTTY_REPO}" "${GHOSTTY_DIR}"
 
-minisign -V -m "ghostty-${GHOSTTY_VERSION}.tar.gz" -P "${PUB_KEY}" -s "ghostty-${GHOSTTY_VERSION}.tar.gz.minisig"
-
-tar -xzmf "ghostty-${GHOSTTY_VERSION}.tar.gz"
-
-rm "ghostty-${GHOSTTY_VERSION}.tar.gz" \
-	"ghostty-${GHOSTTY_VERSION}.tar.gz.minisig"
-
+# Hardcode version
+GHOSTTY_VERSION="1.3.0"
 BUILD_ARGS="${BUILD_ARGS} -Dversion-string=${GHOSTTY_VERSION}"
 
 (
-	cd "ghostty-${GHOSTTY_VERSION}"
-	ZIG_GLOBAL_CACHE_DIR=/tmp/offline-cache ./nix/build-support/fetch-zig-cache.sh
-	zig build ${BUILD_ARGS}
+    cd "${GHOSTTY_DIR}"
+    ZIG_GLOBAL_CACHE_DIR=/tmp/offline-cache ./nix/build-support/fetch-zig-cache.sh
+    zig build ${BUILD_ARGS}
 )
